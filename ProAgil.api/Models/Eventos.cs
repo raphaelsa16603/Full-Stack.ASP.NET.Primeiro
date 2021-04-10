1bdf24460d74cc817066411d2c31fe561b6b3ae3
raphaelsa16603 <raphaelsa16603@gmail.com>
2021-04-10 02:17:29 -0300

    first commit

---
+++ ProAgil.api/Models/Eventos.cs
@@ -0,0 +1,13 @@
+namespace ProAgil.api.Models
+{
+    public class Eventos
+    {
+        public int IdEvento {get; set;}
+        public string NomeEvento {get; set;}
+        public string Local {get; set;}
+        public string Data { get; set;}
+        public int QuantidadeDePessoas {get; set;}
+        public string Lote {get; set;}
+        
+    }
+}
